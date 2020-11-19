5c10c680da4801888087a8cc38e1b9eb2160fc3d
coderInPractice <coderInPractice@noreply.github.com>
2020-11-19 17:12:29 +0530

    Retrofit configured

---
+++ app/src/main/java/com/example/facefilterassignment/api/RetrofitClient.java
@@ -0,0 +1,26 @@
+package com.example.argear.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    public static ContentsApi createContentsService(String baseUrl) {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(httpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ContentsApi.class);
+    }
+}
